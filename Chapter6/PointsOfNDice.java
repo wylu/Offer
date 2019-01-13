995d0c64318f4b996339f6790d44b4db6327db0b
wylu <158677478@qq.com>
2019-01-13 15:31:42 +0800

    2019/1/13

--- Chapter6/PointsOfNDice.java
+++ Chapter6/PointsOfNDice.java
@@ -9,19 +9,20 @@ public class PointsOfNDice {
 
         //骰子最大面值
         int diceMaxVal = 6;
-        int[][] prob = new int[2][diceMaxVal * num + 1];
+        //count[][n]: 点数和为n出现的次数
+        int[][] count = new int[2][diceMaxVal * num + 1];
 
         int flag = 0;
-        for (int i = 1; i <= diceMaxVal; i++) prob[flag][i] = 1;
+        for (int i = 1; i <= diceMaxVal; i++) count[flag][i] = 1;
 
         for (int k = 2; k <= num; k++) {
             //有k个骰子时，最小点数和为k，不存在出现点数和小于k的情况
-            for (int i = 0; i < k; i++) prob[1 - flag][i] = 0;
+            for (int i = 0; i < k; i++) count[1 - flag][i] = 0;
 
             for (int i = k; i <= diceMaxVal * k; i++) {
-                prob[1 - flag][i] = 0;
+                count[1 - flag][i] = 0;
                 for (int j = 1; j < i && j <= diceMaxVal; j++) {
-                    prob[1 - flag][i] += prob[flag][i - j];
+                    count[1 - flag][i] += count[flag][i - j];
                 }
             }
 
@@ -31,11 +32,11 @@ public class PointsOfNDice {
 
         double total = Math.pow(diceMaxVal, num);
         for (int i = num; i <= diceMaxVal * num; i++) {
-            System.out.println(String.format("%d : %.2f", i, prob[flag][i] / total));
+            System.out.println(String.format("%d : %.6f", i, count[flag][i] / total));
         }
     }
 
     public static void main(String[] args) {
-        new PointsOfNDice().printProbability(2);
+        new PointsOfNDice().printProbability(3);
     }
 }
